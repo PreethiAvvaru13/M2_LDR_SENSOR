175180d3e2b58f5ef63e34b112a8812b8d3e72b9
PreethiAvvaru13 <102947832+PreethiAvvaru13@users.noreply.github.com>
2022-04-21 09:57:40 +0530

    Update and rename ldr sensor.c to project_main.c

---
+++ 3_Implementation/project_main.c
@@ -0,0 +1,25 @@
+/**
+ * @file project_main.c
+ * @author A Preethi (avvarupreethi13@gmail.com)
+ * @brief Project to LDR SENSOR
+ * @version 0.1
+ * @date 2022-04-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+# define GREEN 2
+  # define ORANGE 3
+  # define RED 4
+  # define Buzzer 5
+  # define Sensor A0
+    
+ 
+  void main()
+  {
+    void setup();
+    void loop();
+    void status_Indicator();
+  } 
+      
+   
