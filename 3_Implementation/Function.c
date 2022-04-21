c26776325e6f72bd9ca5f121750666a95b69ae6f
PreethiAvvaru13 <102947832+PreethiAvvaru13@users.noreply.github.com>
2022-04-21 09:58:43 +0530

    Create Function.c

---
+++ 3_Implementation/Function.c
@@ -0,0 +1,34 @@
+ void setup() {
+    for (int i = 2; i < 6; i++)
+      pinMode(i, OUTPUT);
+  
+    Serial.begin(9600);
+  }
+  
+  void loop() 
+  {    
+    for (int i = 2; i < 6; i++)
+      digitalWrite(i, LOW);
+  
+    status_Indicator();  
+  }
+  
+  void status_Indicator()
+  {
+    int sensor_In = analogRead(A0);
+    Serial.println(sensor_In);
+    
+    if ( sensor_In >=400  )
+    {
+      digitalWrite(RED, HIGH);
+      digitalWrite(Buzzer,HIGH);
+    }
+    else if (sensor_In >=160 )
+    {
+      digitalWrite(ORANGE, HIGH);
+    } else
+    {
+      digitalWrite(GREEN, HIGH);
+    }
+    delay(10);
+  }
